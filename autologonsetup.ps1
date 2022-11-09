f116a807033baf91a859dbf760dad4bb23eea594
0xAalvarado <117859891+0xAalvarado@users.noreply.github.com>
2022-11-09 13:58:12 -0600

    Add files via upload
    This script will allow you to interactively create an auto login for the machine via modifying the Registry settings

---
+++ autologonsetup.ps1
@@ -0,0 +1,8 @@
+﻿$Username = Read-Host 'Enter username for auto-logon'
+$Pass = Read-Host "Enter password for $Username"
+$Domain = Read-Host 'Enter the Domain of your Organization'
+$RegistryPath = 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+Set-ItemProperty $RegistryPath 'AutoAdminLogon' -Value "1" -Type String 
+Set-ItemProperty $RegistryPath 'DefaultUsername' -Value "$Username" -type String 
+Set-ItemProperty $RegistryPath 'DefaultPassword' -Value "$Pass" -type String
+set-Itemproperty $RegistryPath 'DefaultDomainName' -Value "$Domain" -type String 
