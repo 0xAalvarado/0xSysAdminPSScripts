9f71f22c9c5665b1b1193e10fccfdec3849f2aee
0xAalvarado <117859891+0xAalvarado@users.noreply.github.com>
2022-11-09 14:39:43 -0600

    Map a Shared Drive w/ PS
    This script will allow you to map a shared network drive with power shell

--- Map_Network_Drive.ps1
+++ Map_Network_Drive.ps1
@@ -4,5 +4,3 @@ $User = "domain\username"
 $PWord = ConvertTo-SecureString -String "password_goes_here" -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $PWord
 New-PSDrive -Name "K" -PSProvider "FileSystem" -Root \\UNCPathGoesHere -Credential $Credential -Persist
-
-
